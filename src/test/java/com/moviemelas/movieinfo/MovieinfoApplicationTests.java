f4895ad7f7d19f1768351c87a3244ea51c3b6c8d
Shubham <shubhamm2693@gmail.com>
2020-08-18 00:07:31 +0530

    MOVIE-INFO-MICROSERVICE-INITIATED

---
+++ src/test/java/com/moviemelas/movieinfo/MovieinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviemelas.movieinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
